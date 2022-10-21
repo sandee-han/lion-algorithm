df4b125d17ca535b850301f7b4f13afd90cb0ea9
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-21 10:07:15 +0900

    solve bracket without stack

---
+++ src/main/java/bracket/SolveBracket.java
@@ -0,0 +1,11 @@
+package bracket;
+
+public class SolveBracket {
+    public boolean solution(String s) {
+        while (s.indexOf("()") >= 0) {
+            s = s.replace("()", "");
+        }
+        return s.length() == 0;
+    }
+
+}
