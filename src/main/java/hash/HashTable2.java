451160caa97f9c7a186b521df3aa28da9a181dab
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-27 10:41:55 +0900

    완주하지 못한 선수 풀이 + diy

--- src/main/java/hash/HashTable2.java
+++ src/main/java/hash/HashTable2.java
@@ -1,74 +1,74 @@
-//package hash;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class HashTable2 {
-//    // key, value를 저장하기 위한 Object
-//    class Node {
-//        private String key;
-//        private Integer value;
-//
-//        public Node(String key, Integer value) {
-//            this.key = key;
-//            this.value = value;
-//        }
-//        public String getKey() {
-//            return key;
-//        }
-//        public Integer getValue() {
-//            return value;
-//        }
-//    }
-//    int size = 10000;
-//    List<Node>[] table = new ArrayList[1000];
-//
-//    public HashTable2() {
-//    }
-//
-//    public HashTable2(int size) {
-//        this.size = size;
-//        this.table = new ArrayList[size];
-//    }
-//
-//    public int hash(String key) {
-//        int asciiSum = 0;
-//        for (int i = 0; i < key.length(); i++) {
-//            asciiSum += key.charAt(i);
-//        }
-//        return asciiSum % 90;
-//    }
-//
-//    public void insert(String key, int value) {
-//        // List
-//        int hashIdx = hash(key);
-//        if(this.table[hashIdx] == null) {
-//            this.table[hashIdx] = new ArrayList<>();    // 데이터를 넣은 적이 없으면 null이므로 ArrayList생성
-//        }
-//        //  key, value를 같이 넣기 위해서 Map 을 쓰거나 / Object를 만들거나
-//        this.table[hashIdx].add(new Node(key, value));
-//    }
-//
-//    public Integer get(String key) {
-//        List<Node> nodes = this.table[hash(key)];
-//        for (Node node : nodes) {
-//            if(key.equals(node.getKey())) {
-//                return node.value;
-//            }
-//        }
-//        return null;
-//    }
-//
-//    public static void main(String[] args) {
-//        HashTable2 ht = new HashTable2();
-//        ht.insert("Yoonseo", 1);
-//        ht.insert("SeoYoon", 2);
-//
-//        int result = ht.get("Yoonseo");
-//        if(result == 1) {
-//            System.out.println("test success");
-//        } else {
-//            System.out.println("test fail");
-//        }
-//    }
-//}
+package hash;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashTable2 {
+    // key, value를 저장하기 위한 Object
+    class Node {
+        private String key;
+        private Integer value;
+
+        public Node(String key, Integer value) {
+            this.key = key;
+            this.value = value;
+        }
+        public String getKey() {
+            return key;
+        }
+        public Integer getValue() {
+            return value;
+        }
+    }
+    int size = 10000;
+    List<Node>[] table = new ArrayList[1000];
+
+    public HashTable2() {
+    }
+
+    public HashTable2(int size) {
+        this.size = size;
+        this.table = new ArrayList[size];
+    }
+
+    public int hash(String key) {
+        int asciiSum = 0;
+        for (int i = 0; i < key.length(); i++) {
+            asciiSum += key.charAt(i);
+        }
+        return asciiSum % 90;
+    }
+
+    public void insert(String key, int value) {
+        // List
+        int hashIdx = hash(key);
+        if(this.table[hashIdx] == null) {
+            this.table[hashIdx] = new ArrayList<>();    // 데이터를 넣은 적이 없으면 null이므로 ArrayList생성
+        }
+        //  key, value를 같이 넣기 위해서 Map 을 쓰거나 / Object를 만들거나
+        this.table[hashIdx].add(new Node(key, value));
+    }
+
+    public Integer get(String key) {
+        List<Node> nodes = this.table[hash(key)];
+        for (Node node : nodes) {
+            if(key.equals(node.getKey())) {
+                return node.value;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        HashTable2 ht = new HashTable2();
+        ht.insert("Yoonseo", 1);
+        ht.insert("SeoYoon", 2);
+
+        int result = ht.get("Yoonseo");
+        if(result == 1) {
+            System.out.println("test success");
+        } else {
+            System.out.println("test fail");
+        }
+    }
+}
--- src/main/java/hash/diy/SolutionDIY.java
+++ src/main/java/hash/diy/SolutionDIY.java
@@ -1,14 +1,14 @@
-package hash;
+package hash.diy;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
+public class SolutionDIY {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> memo = new HashMap<>();
         for (int i = 0; i < participant.length; i++) {
             String key = participant[i];
-            if (!memo.containsKey(key)) {
+            if(!memo.containsKey(key)) {
                 memo.put(key, 0);
             }
             memo.put(key, memo.get(key) + 1);
@@ -18,10 +18,10 @@ public class Solution {
             memo.put(key, memo.get(key) - 1);
         }
         for (String key : memo.keySet()) {
-            if (memo.get(key) == 1) {
+            if(memo.get(key) == 1 ){
                 return key;
             }
         }
-        return "";
+        return null;
     }
 }
