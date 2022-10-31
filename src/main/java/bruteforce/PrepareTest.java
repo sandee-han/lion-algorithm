d3855de3730e9112328f48e9acd9a36942357078
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-31 10:41:34 +0900

    PrepareTest solve with array

--- src/main/java/bruteforce/PrepareTest.java
+++ src/main/java/bruteforce/PrepareTest.java
@@ -1,30 +1,41 @@
 package bruteforce;
 
+import java.util.Arrays;
+
 public class PrepareTest {
-    class Solution {
         public int[] solution(int[] answers) {
             /*
             1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
             2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
             3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+
+            1. 공간복잡도로 속도 높이는 방법
+            -> Compare with String
+
+            2. 나머지를 이용하는 방법
+            -> Array
+
              */
+            int[][] students = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
 
-//            To Do
-//            1번 학생 방식으로 배열에 입력
-//            이중 for문으로 하나씩 비교 O(N^2) - 아닐거같음
-//            정답 확인 후 맞은 갯수 리턴
+            int[] memo = {0, 0, 0};
+
+            for (int i = 0; i < answers.length; i++) {
+                for (int j = 0; j < memo.length; j++) {
+                    if (answers[i] == students[j][i % students[j].length]) memo[j] += 1;
+                }
+            }
+            System.out.println(Arrays.toString(memo));
             
-
-            
-
-
-
-
             return new int[0];
         }
-    }
+
 
     public static void main(String[] args) {
+        int[] answer = {1, 2, 3, 4, 5};
+        int[] answer2 = {1, 3, 2, 4, 2};
 
+        PrepareTest prepareTest = new PrepareTest();
+        prepareTest.solution(answer);
     }
 }
