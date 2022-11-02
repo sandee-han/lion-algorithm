a1755aabe259bbf3218d8b35c8eda975c4be653e
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-02 09:41:03 +0900

    2 ~ 49 값 리스트에 삽입. 2의 배수 항목 지움. 출력

--- src/main/java/primenumber/RemoveMultipleOf.java
+++ src/main/java/primenumber/RemoveMultipleOf.java
@@ -1,6 +1,43 @@
 package primenumber;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RemoveMultipleOf {
+
     int N = 50;
+
+    public ArrayList<Integer> putNumber(ArrayList<Integer> table) {
+        for (int i = 2; i < 50; i++) {
+            table.add(i);
+        }
+        return table;
+    }
+
+    public ArrayList<Integer> removeMultipleOfTwo(ArrayList<Integer> table) {
+        for (int i = 2; i < table.size(); i++) {
+            if (table.get(i) % 2 == 0) {
+                table.remove(i);
+            }
+        }
+        return table;
+    }
+
+    public void show(ArrayList<Integer> table) {
+        System.out.println("size of table = " + table.size());
+        for (Integer integer : table) {
+            System.out.print(integer + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int N = 50;
+        ArrayList<Integer> table = new ArrayList<>();
+
+        RemoveMultipleOf rmo = new RemoveMultipleOf();
+        rmo.putNumber(table);
+        rmo.removeMultipleOfTwo(table);
+        rmo.show(table);
+    }
 
 }
