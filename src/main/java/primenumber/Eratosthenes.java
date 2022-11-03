a23c8f0002a1dcda0a436eea04d61ddb93488c8f
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-03 09:34:20 +0900

    에라토스테네스의 체. 일단 값 출력만

--- src/main/java/primenumber/Eratosthenes.java
+++ src/main/java/primenumber/Eratosthenes.java
@@ -2,17 +2,15 @@ package primenumber;
 
 public class Eratosthenes {
     public static void main(String[] args) {
-        int num = 2;
-        int check = 2;
+        int num = 1;
+        int check = 0;
+
         for (int i = 2; i <= 100; i++) {
-            System.out.printf("%d ", num);
+            check +=2;
             num++;
+            System.out.printf("num: %d , check: %d\n", num, check);
         }
-        System.out.println();
-        for (int i = 2; i <= 102; i+=2) {
-            System.out.printf("%d ", check);
-            check = i;
-        }
+
 
     }
 }
