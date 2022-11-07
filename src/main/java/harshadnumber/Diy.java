2c2dbde8baeac6a3fcbce1cbda316c74aa121254
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-07 09:33:25 +0900

    harshad 완성

--- src/main/java/harshadnumber/Diy.java
+++ src/main/java/harshadnumber/Diy.java
@@ -1,25 +1,26 @@
 package harshadnumber;
 
 public class Diy {
+
+    class Solution {
+        public boolean solution(int x) {
+            int check = x;
+            int sum = 0;
+
+            while (x > 0) {
+                sum += x % 10;
+                x /= 10;
+            }
+
+            if(check % sum == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        // 1. 자릿수 나누고 값 더하기
-        // 2. 더한 값 나누기
-        int x = 12;
-        int check = x;
-        int sum = 0;
-        boolean answer;
 
-        while (x > 0) {
-            sum += x % 10;
-            x /= 10;
-        }
-
-        if(check % sum == 0) {
-            answer = true;
-        } else {
-            answer = false;
-        }
-        System.out.println(sum);
-        System.out.println(answer);
     }
 }
