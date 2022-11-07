51ac51304331cb0f18632c159921bdacd2e6d948
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-07 09:50:11 +0900

    list.isEmpty true >> list.add(-1)

--- src/main/java/arraywithdivisor/ArrayWithDivisorDiy.java
+++ src/main/java/arraywithdivisor/ArrayWithDivisorDiy.java
@@ -8,11 +8,15 @@ public class ArrayWithDivisorDiy {
     public static void main(String[] args) {
         int[] arr = {5, 9, 7 ,10};
         List<Integer> list = new ArrayList<>();
-        int divisor = 1;
+        int divisor = 4;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % divisor == 0) list.add(arr[i]);
         }
-        Collections.sort(list);
+        if(list.isEmpty()) {
+            list.add(-1);
+        } else {
+            Collections.sort(list);
+        }
         System.out.println(list);
     }
 }
