20ea1b0b8b9610b83d88b462d185420cdc1568b1
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-07 10:18:33 +0900

    ArrayWithDivisorDiy complete

--- src/main/java/arraywithdivisor/ArrayWithDivisorDiy.java
+++ src/main/java/arraywithdivisor/ArrayWithDivisorDiy.java
@@ -1,10 +1,29 @@
 package arraywithdivisor;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class ArrayWithDivisorDiy {
+
+    public int[] solution(int[] arr, int divisor) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) list.add(arr[i]);
+        }
+        if(list.isEmpty()) {
+            list.add(-1);
+        } else {
+            Collections.sort(list);
+        }
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
         int[] arr = {5, 9, 7 ,10};
         List<Integer> list = new ArrayList<>();
@@ -17,6 +36,12 @@ public class ArrayWithDivisorDiy {
         } else {
             Collections.sort(list);
         }
-        System.out.println(list);
+
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+
+        System.out.println(Arrays.toString(answer));
     }
 }
