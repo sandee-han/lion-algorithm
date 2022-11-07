f372e3558217728444cfa54552546b3d30a4817c
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-07 09:20:27 +0900

    step 2. 더한 값 나누고 harshad number 확인

--- src/main/java/harshadnumber/Diy.java
+++ src/main/java/harshadnumber/Diy.java
@@ -4,14 +4,22 @@ public class Diy {
     public static void main(String[] args) {
         // 1. 자릿수 나누고 값 더하기
         // 2. 더한 값 나누기
-        int x = 1222;
+        int x = 12;
+        int check = x;
         int sum = 0;
-        int cnt = 0;
+        boolean answer;
 
         while (x > 0) {
             sum += x % 10;
             x /= 10;
         }
+
+        if(check % sum == 0) {
+            answer = true;
+        } else {
+            answer = false;
+        }
         System.out.println(sum);
+        System.out.println(answer);
     }
 }
