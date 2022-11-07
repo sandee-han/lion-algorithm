aa1173edddc537aa56ffab3ed2fba80a29172f87
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-07 09:10:18 +0900

    harshad number step 1, 2

---
+++ src/main/java/harshadnumber/Diy.java
@@ -0,0 +1,11 @@
+package harshadnumber;
+
+public class Diy {
+    public static void main(String[] args) {
+        // 1. 자릿수 나누고 값 더하기
+        // 2. 더한 값 나누기
+        int x = 12;
+        int sum = 0;
+
+    }
+}
--- src/main/java/primenumber/Eratosthenes.java
+++ src/main/java/primenumber/Eratosthenes.java
@@ -1,6 +1,8 @@
 package primenumber;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Eratosthenes {
     public static void main(String[] args) {
@@ -12,15 +14,34 @@ public class Eratosthenes {
         for (int i = 0; i < nums.length; i++) nums[i] = i + 2; // 2~50까지 nums에 채우기
 
         // 2의 배수 지우기
-        int multipleOf = 2;
-        for (int i = 0; i < nums.length; i+=2) {
-            checks[i] = false;
-        }
+        // nums[j] + j
+        for (int j = 0; j * j <= N; j++) {
+            System.out.println(j);
+            int multipleOf = nums[j];
+            // 초항이 2, 2n 인 2 4 6 8 등차수열
+            for (int i = nums[j] + j; i < nums.length; i+=multipleOf) {
+                checks[i] = false;
+            }
+       }
 
         // checks를 참고해서 true인 친구들만 nums[checks[i]]만 출력
+        int cnt = 0;
         for (int i = 0; i < nums.length; i++) {
-            if(checks[i] == true) System.out.printf("%d \t", nums[i]);
+            if(checks[i] == true) {
+                System.out.printf("%d\t", nums[i]);
+                cnt++;
+            }
         }
+        System.out.printf("\n%d", cnt);
+
+
+
+        // List
+//        List<Integer> primes = new ArrayList<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            if(checks[i] == true) primes.add(nums[i]);
+//        }
+//        System.out.println(primes);
 
     }
 }
