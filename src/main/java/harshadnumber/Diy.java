05e6a91c4ce23617bb822248e733f5a998e64507
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-07 09:18:19 +0900

    step 1. 자릿수 나누고 값 더하기

--- src/main/java/harshadnumber/Diy.java
+++ src/main/java/harshadnumber/Diy.java
@@ -4,8 +4,14 @@ public class Diy {
     public static void main(String[] args) {
         // 1. 자릿수 나누고 값 더하기
         // 2. 더한 값 나누기
-        int x = 12;
+        int x = 1222;
         int sum = 0;
+        int cnt = 0;
 
+        while (x > 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+        System.out.println(sum);
     }
 }
