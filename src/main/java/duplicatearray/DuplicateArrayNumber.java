b3da9a3e6fe8c1b71f10a1bcdd26b02a67b4e581
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-10 09:16:14 +0900

    같은숫자는싫어 queue에 입력 후 출력

--- src/main/java/duplicatearray/DuplicateArrayNumber.java
+++ src/main/java/duplicatearray/DuplicateArrayNumber.java
@@ -1,5 +1,6 @@
 package duplicatearray;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -7,14 +8,28 @@ public class DuplicateArrayNumber {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 1, 3, 3, 0, 1, 1};
         Queue<Integer> que = new LinkedList<>();
-        
-        //  arr[0] 그냥 넣고
-        
-        
-        // arr[1] 넣기전 이전 값과 동일한지 확인
-        
-        
-        // arr[2] 넣기 전 이전 값과 동일한지 확인
 
+        //  arr[0] 그냥 넣고
+        que.offer(arr[0]);
+
+//        // arr[1] 넣기전 이전 값과 동일한지 확인
+//        if (arr[1] != arr[0]) que.offer(arr[1]);
+//
+//        // arr[2] 넣기 전 이전 값과 동일한지 확인
+//        if (arr[2] != arr[1]) que.offer(arr[2]);
+
+        // for문으로 정리
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) que.offer(arr[i]);
+        }
+
+        Iterator iter = que.iterator();
+
+        System.out.println("[");
+        while (iter.hasNext())
+            System.out.print(iter.next() + " ");
+        System.out.println("]");
     }
+
 }
+
