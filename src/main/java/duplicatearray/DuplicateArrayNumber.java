2698abf2a04f35f3c1c2b592a8e5fac397c9d5ba
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-10 09:08:31 +0900

    같은숫자는싫어 steps

---
+++ src/main/java/duplicatearray/DuplicateArrayNumber.java
@@ -0,0 +1,20 @@
+package duplicatearray;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class DuplicateArrayNumber {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 1, 3, 3, 0, 1, 1};
+        Queue<Integer> que = new LinkedList<>();
+        
+        //  arr[0] 그냥 넣고
+        
+        
+        // arr[1] 넣기전 이전 값과 동일한지 확인
+        
+        
+        // arr[2] 넣기 전 이전 값과 동일한지 확인
+
+    }
+}
--- src/main/java/sort/SelectionSort.java
+++ src/main/java/sort/SelectionSort.java
@@ -1,6 +1,8 @@
 package sort;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 interface StatementStrategy {
     boolean apply(int a, int b);
@@ -38,9 +40,49 @@ public class SelectionSort {
         return arr;
     }
 
+    public int[] selectionSortAscBiFunction(int[] arr, BiFunction<Integer, Integer, Boolean> bf) {
+        int tmp = 0;
+        System.out.println("selectionSort with BiFunction");
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i; j < arr.length; j++) {
+                if (bf.apply(arr[minIdx], arr[j])) minIdx = j;
+            }
+            tmp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = tmp;
+            System.out.println(Arrays.toString(arr));
+        }
+        return arr;
+    }
+
+    public int[] selectionSortAscFunction(int[] arr, Function<int[], Boolean> func) {
+        int tmp = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i; j < arr.length; j++) {
+                if (func.apply(arr)) minIdx = j;
+            }
+            tmp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = tmp;
+            System.out.println(Arrays.toString(arr));
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
-        int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+        int[] arr = new int[]{7, 2, 4, 9, 10, 223, 111, 23, 3, 39};
         SelectionSort ss = new SelectionSort();
-        ss.selectionSortAsc(arr, (a, b) -> a > b);
+        ss.selectionSortAscFunction(arr, (arr1) -> arr1[0] > arr1[1]);
+
+//        ss.selectionSortAsc(arr, (a, b) -> a > b);
+
+//        ss.selectionSortAscBiFunction(arr, (a, b) -> a > b);
+
+        Function<Integer[], Boolean> fn = (arr1) -> arr1[0] > arr1[1];
+        System.out.println(fn.apply(new Integer[]{10, 20}));
+
+//        BiFunction<Integer, Integer, Boolean> biFunction = (a, b) -> a > b;
     }
 }
