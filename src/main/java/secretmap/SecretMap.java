63652e7d338ee815487309e98ae19b2b78e27cc8
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-11 09:29:43 +0900

    2진수 변환 toBinaryString 안쓰고

--- src/main/java/secretmap/SecretMap.java
+++ src/main/java/secretmap/SecretMap.java
@@ -17,17 +17,34 @@ public class SecretMap {
 
      */
 
+    public String convertToBinary1(int num) {
+        String binaryNum = Integer.toBinaryString(num);
+        return binaryNum;
+    }
+
+    public String convertToBinaryDiy(int num) {
+        String binaryNum = "";
+        while(num > 0) {
+            binaryNum = Integer.toString(num % 2) + binaryNum;
+            num /= 2;
+        }
+
+        return binaryNum;
+    }
+
     public static void main(String[] args) {
+        SecretMap sm = new SecretMap();
+
         int n = 5;
         int[] arr1 = {9, 20, 28, 18, 11};
         int[] arr2 = {30, 1, 21, 17, 28};
 
-        String numBinary = Integer.toBinaryString(arr1[0]);
+        String numBinary = Integer.toBinaryString(arr1[1]);
         String[] arr1Binary = numBinary.split("");
 
         System.out.println(Arrays.toString(arr1Binary));
         // 자릿수가 모자라면 어떻게 채우는가
-
+        System.out.println(sm.convertToBinaryDiy(20));
 
 
     }
