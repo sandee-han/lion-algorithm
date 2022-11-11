eee94352151598ba2052ab3a9e7aef1c64daabd8
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-11 09:13:41 +0900

    secretMap 목표

--- src/main/java/duplicatearray/DuplicateArrayNumber.java
+++ src/main/java/duplicatearray/DuplicateArrayNumber.java
@@ -1,13 +1,10 @@
 package duplicatearray;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class DuplicateArrayNumber {
 
-    public int[] solution(int []arr) {
+    public int[] solutionWithQueue(int []arr) {
 
         Queue<Integer> que = new LinkedList<>();
 
@@ -24,11 +21,31 @@ public class DuplicateArrayNumber {
         return answer;
     }
 
+    public int[] solutionWithStack(int []arr) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) stack.push(arr[i]);
+        }
+        int[] answer = new int[stack.size()];
+
+        for (int i = answer.length - 1; i >= 0; i--) {
+            answer[i] = stack.peek();
+            stack.pop();
+        }
+
+        return answer;
+    }
+
+
     public static void main(String[] args) {
         int[] arr = new int[]{1, 1, 3, 3, 0, 1, 1};
         DuplicateArrayNumber dan = new DuplicateArrayNumber();
-        int[] test = dan.solution(arr);
-        System.out.println(Arrays.toString(test));
+//        int[] test = dan.solutionWithQueue(arr);
+        int[] testStack = dan.solutionWithStack(arr);
+        System.out.println(Arrays.toString(testStack));
 
     }
 
---
+++ src/main/java/secretmap/SecretMap.java
@@ -0,0 +1,26 @@
+package secretmap;
+
+public class SecretMap {
+
+    /*
+    a1, b1 숫자를 이진수로 변환
+
+    a1, b1 배열로 변환
+
+    숫자 한 자리씩 || 처리 - 오래걸릴거같다
+
+     ~ an, bn 까지 동일하게 || 처리
+
+     1인 경우 #, 0인 경우 " " 출력
+
+     */
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        String nBinary = Integer.toBinaryString(n);
+
+
+    }
+
+}
