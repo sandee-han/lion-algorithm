9448e190cf7ff11168b2b3d4e70cc50414ffc2a1
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-14 10:17:42 +0900

    에라토스테네스의 체: check배열 j의 배수 index false로. (j*j < N 까지)

--- src/main/java/primenumber/EratosthenesReview.java
+++ src/main/java/primenumber/EratosthenesReview.java
@@ -5,18 +5,23 @@ import java.util.Arrays;
 public class EratosthenesReview {
     public static void main(String[] args) {
         int N = 50;
+        // 배열 크기 2 부터 50까지 49
         int[] arr = new int[N - 1];
         boolean[] check = new boolean[arr.length];
         Arrays.fill(check, true);
+        // arr 에 2부터 50까지 입력
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i + 2;
         }
 
-        for (int i = 1; i < N - 1; i++) {
-            if (arr[i] % 2 == 0) {
-                check[i] = false;
+        for(int j = 2; j * j < N; j++) {
+            for (int i = j; i < N - 1; i++) {
+                if (arr[i] % j == 0) {
+                    check[i] = false;
+                }
             }
         }
+
         System.out.println(Arrays.toString(arr));
         System.out.println(Arrays.toString(check));
     }
