05244e665157fff1d4df003e29af0e657afad730
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-17 10:29:44 +0900

    QuickSortWithArray 완성..

--- src/main/java/sort/QuickSortWithArray.java
+++ src/main/java/sort/QuickSortWithArray.java
@@ -1,28 +1,37 @@
 package sort;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class QuickSortWithArray {
 
     public int[] sort(int[] arr, int startIdx, int endIdx) {
-        int pivot = arr[arr.length / 2];
-        int leftIdx = 0;
-        int rightIdx = arr.length - 1;
+        int pivot = arr[(startIdx + endIdx) / 2];
+        int leftIdx = startIdx;
+        int rightIdx = endIdx;
         int tmp;
 
-        while (arr[leftIdx] < pivot) leftIdx += 1;
-        while (arr[rightIdx] > pivot) rightIdx -= 1;
-        tmp = arr[leftIdx];
-        arr[leftIdx] = arr[rightIdx];
-        arr[rightIdx] = tmp;
-        leftIdx += 1;
-        rightIdx -= 1;
+        while (leftIdx <= rightIdx) {
+            while (arr[leftIdx] < pivot) leftIdx += 1;
+            while (arr[rightIdx] > pivot) rightIdx -= 1;
 
-
+            if(leftIdx<=rightIdx) {
+                tmp = arr[leftIdx];
+                arr[leftIdx] = arr[rightIdx];
+                arr[rightIdx] = tmp;
+                leftIdx += 1;
+                rightIdx -= 1;
+            }
+        }
+        if(startIdx < rightIdx) sort(arr, startIdx, rightIdx);
+        if(leftIdx < endIdx) sort(arr, leftIdx, endIdx);
 
         return arr;
     }
 
     public static void main(String[] args) {
         var arr = new int[]{20, 18, 5, 19, 5, 25, 40, 50};
-
+        QuickSortWithArray quickSortWithArray = new QuickSortWithArray();
+        System.out.println(Arrays.toString(quickSortWithArray.sort(arr, 0, arr.length -1)));
     }
 }
