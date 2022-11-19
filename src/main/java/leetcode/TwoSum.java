8fcdafca4e2bce0321e4123e0a601691ce6efe9f
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-20 01:39:14 +0900

    TwoSum. 두번째 도전. Hint: Map

--- src/main/java/leetcode/TwoSum.java
+++ src/main/java/leetcode/TwoSum.java
@@ -1,11 +1,22 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class TwoSum {
+    // 2트
+    // Map을 쓰라는 힌트를 봤다.
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(i, target - nums[i]);
+        }
+
+        return nums;
+    }
+
+    // List의 contains 사용
+    // 매우 느리다.
+    public int[] twoSum2(int[] nums, int target) {
         int[] answer = new int[2];
 
         // .contains() 를 사용하기 위해서 입력 배열 arr를 List로 바꿔준다.
@@ -14,13 +25,24 @@ public class TwoSum {
             numsList.add(num);
         }
 
-        // i + 1 조건을 위해 length - 1 처리. 어차피 마지막 값은 처리하지 않아도 괜찮다.
-        for (int i = 0; i < nums.length - 1; i++) {
-            // target이 nums[i]의 두배이면서, nums[i+1]도 같은 값인 경우 ex. {3, 3}, target: 6
-            if (target == nums[i] * 2 && target == nums[i + 1] * 2) {
-                answer[0] = i;
-                answer[1] = i + 1;
+        // List에 몇 개 있는지 확인하기 위한 count
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            // target / 2 가 nums에 있을 경우, answer[]에 추가 및 count++
+            if (target / 2 == nums[i]) {
+                answer[count] = i;
+                count++;
             }
+        }
+
+        // count 가 2면 return
+        // 아닐경우 원래 조건문으로
+        if (count == 2) {
+            return answer;
+        }
+
+        // 마지막 값은 처리하지 않아도 괜찮다.
+        for (int i = 0; i < nums.length - 1; i++) {
             // nums[i]를 뺀 값이 배열에 존재하는지 확인.
             // target이 nums[i]의 두배이면서 nums에 하나밖에 없느 경우 제외
             if (numsList.contains(target - nums[i]) && target != nums[i] * 2) {
@@ -34,15 +56,19 @@ public class TwoSum {
 
     public static void main(String[] args) {
         TwoSum ts = new TwoSum();
-        int[] arr1 = {2, 7, 11, 15};
-        int[] arr2 = {3, 2, 4};
+        int[] arr1 = {11, 2, 7, 15}; // target: 9
+        int[] arr2 = {3, 2, 4}; // target: 6
         int[] arr3 = {3, 3};
+        int[] arr4 = {3, 2, 3};
 
         int[] test1 = ts.twoSum(arr1, 9);
         int[] test2 = ts.twoSum(arr2, 6);
         int[] test3 = ts.twoSum(arr3, 6);
+        int[] test4 = ts.twoSum(arr4, 6);
+
         System.out.println(Arrays.toString(test1));
         System.out.println(Arrays.toString(test2));
         System.out.println(Arrays.toString(test3));
+        System.out.println(Arrays.toString(test4));
     }
 }
