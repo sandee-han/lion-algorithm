281aff490f145e42ffe832e64e0503c67ae26af8
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-22 09:24:58 +0900

    recursiv. 탈출조건, 1씩 증가

--- src/main/java/recursive/Print1To100.java
+++ src/main/java/recursive/Print1To100.java
@@ -2,8 +2,9 @@ package recursive;
 
 public class Print1To100 {
     public static void print1To100(int n) {
+        if(n > 100) return;
         System.out.println(n);
-        print1To100(n);
+        print1To100(n + 1);
     }
 
     public static void main(String[] args) {
