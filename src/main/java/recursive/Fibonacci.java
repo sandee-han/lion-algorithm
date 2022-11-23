e5d7108e38627375ede755ed9f74bb3f15e7e95f
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-23 10:44:21 +0900

    Recursive Fibonacci without recursive

--- src/main/java/recursive/Fibonacci.java
+++ src/main/java/recursive/Fibonacci.java
@@ -17,6 +17,26 @@ public class Fibonacci {
         return fibonacciNums.get(num - 1);
     }
 
+    int[] countWithoutRec(int num) {
+        int[] arr = new int[num];
+        if (num == 1) {
+            arr[0] = 1;
+            return arr;
+        }
+
+        if (num == 2) {
+            arr[0] = 1;
+            arr[1] = 1;
+            return arr;
+        }
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = arr[i - 2] + arr[i - 1];
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
 
     }
