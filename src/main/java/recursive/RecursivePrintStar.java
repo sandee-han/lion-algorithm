ff9da7c92ab961af1318eb0f0d41f48d1e2196fc
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-24 09:43:03 +0900

    재귀를 이용한 별찍기. 1. 별 출력, 2. 줄바꿈 출력. 탈출 조건 정의 필요

--- src/main/java/recursive/RecursivePrintStar.java
+++ src/main/java/recursive/RecursivePrintStar.java
@@ -1,6 +1,8 @@
 package recursive;
 
 public class RecursivePrintStar {
+
+    static int count = 1;
     public static int printStar(int n) {
         System.out.print("*");
         if (n == 1) {
@@ -9,8 +11,17 @@ public class RecursivePrintStar {
         return printStar(n - 1);
     }
 
+    public static int printLine(int n) {
+        printStar(count);
+        System.out.printf("\n%d 출력: ", count + 1);
+        printLine(count++);
+        if (count > n) return 0;
+        return 0;
+    }
+
     public static void main(String[] args) {
         int input = 5;
-        printStar(input);
+        printLine(input);
     }
+
 }
