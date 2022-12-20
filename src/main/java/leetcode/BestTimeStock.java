3929e1fd2b74bc0a0e27deb5ebf97d36722ee392
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-12-20 23:34:57 +0900

    LeetCode BestTimetoBuyandSellStock - wrong solution2

--- src/main/java/leetcode/BestTimeStock.java
+++ src/main/java/leetcode/BestTimeStock.java
@@ -36,6 +36,7 @@ public class BestTimeStock {
     public static void main(String[] args) {
         int[] prices1 = {7,1,5,3,6,4};
         int[] prices2 = {3,2,6,5,0,3};
+
         System.out.println(maxProfit(prices2));
     }
 }
